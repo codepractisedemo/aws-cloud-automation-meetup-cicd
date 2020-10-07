9c411ddffce18b741fe8cb15bfdffcd7a260c1a8
Jagan Rajagopal <askjaganr@gmail.com>
2020-10-07 22:59:48 +0530

    adding the changes files

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 #
 # the name of our application - this will show up in the tag "Application" tag
 # for all AWS resources that we create
-#
+##
 variable "application_name" {
   type = string
   default = "cool_webapp"
